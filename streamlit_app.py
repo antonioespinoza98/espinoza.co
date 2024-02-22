7b838865b0bc426c2c15f79262349d7034561b27
antonioespinoza98 <espinozamarco70@gmail.com>
2024-02-22 20:13:37 -0300

    cambios

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ with tab1:
     st.markdown('+ Análisis de regresión')
     
 with tab2: 
+    # Quienes somos
     st.header('¿Quiénes somos?')
     col1, col2 = st.columns(2)
     with col1:
@@ -21,8 +22,21 @@ with tab2:
     with col2:
         st.subheader("M.Sc Humberto Espinoza")
         st.write("Con más de 30 años de experiencia en el área médica (...)")
-    
-    
+    # Misión
+    st.header('Misión')
+    st.write('Nuestro propósito es ser un agente mediador entre la estadística y el o la investigadora para traer una armonía metodológica y que a su vez, sea comunicada de la mejor manera.')
+    # Visión
+    st.header('Visión')
+    st.write('Consolidarnos como la mejor consultora en investigaciones en múltiples disciplinas.')
+    # Valores
+    st.header('Valores')
+    st.write('Como empresa familiar, nuestros valores significan lo que nos ha caracterizado por generaciones. Estos valores no solo definen quiénes somos como empresa, sino que también informan nuestras acciones y decisiones en cada interacción con nuestros clientes, socios y colegas. Con disciplina, organización y excelencia como pilares fundamentales, estamos comprometidos a alcanzar nuevas alturas de éxito y hacer una diferencia significativa en el mundo que nos rodea.')
+    st.subheader('Disciplina')
+    st.write('La disciplina es el cimiento sobre el cual construimos nuestro éxito. Nos comprometemos a mantenernos enfocados en nuestras metas y objetivos, incluso cuando los desafíos parezcan insuperables. A través de la disciplina, demostramos nuestra dedicación y determinación para alcanzar la excelencia en todo lo que hacemos.')
+    st.subheader('Organización')
+    st.write('La organización es la clave para la eficiencia y la efectividad. Nos esforzamos por mantener un entorno de trabajo ordenado y estructurado, donde cada tarea y proceso esté claramente definido y coordinado. Al priorizar la organización, optimizamos nuestro rendimiento y garantizamos resultados consistentes y confiables.')
+    st.subheader('Excelencia')
+    st.write('La excelencia es nuestro estándar. Buscamos constantemente superar las expectativas y alcanzar la más alta calidad en cada proyecto, producto o servicio que entregamos. Nos negamos a conformarnos con lo ordinario, y en su lugar, aspiramos a la grandeza en todo lo que emprendemos.')
 with tab3:
 
     with st.form('contacto'):
