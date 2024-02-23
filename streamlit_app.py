b14282ddbcbb0efe1ee7d477eba308a36ba8d88e
antonioespinoza98 <espinozamarco70@gmail.com>
2024-02-22 21:24:05 -0300

    pág principal

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-st.set_page_config(page_title= 'Espinoza')
+st.set_page_config(page_title= 'DataEx',
+                   page_icon= ':globe_with_meridians:')
 
 tab1, tab2, tab3, tab4 = st.tabs(['Nuestros servicios','Sobre nosotros','Contáctenos', 'Tecnologías'])
 
 with tab1:
+    st.title('DataEx')
+    st.write('***Convirtiendo información en innovación***')
     st.header('Nuestros servicios')
+    st.image(".\imagenes\page1.jpg")
+    st.caption("Photo by Clay Banks on Unsplash")
     st.markdown('Nuestro enfoque integral pretende brindar un servicio integral con una fuerte influencia cuantitativa, junto con la tecnología más moderna del mercado.')
     st.markdown('+ Muestreo')
     st.markdown('+ Análisis de series temporales')
@@ -13,15 +18,15 @@ with tab1:
     
 with tab2: 
     # Quienes somos
-    st.header('¿Quiénes somos?')
+    st.header('Nuestro equipo')
     col1, col2 = st.columns(2)
     with col1:
-        st.subheader("B.Sc Marco Espinoza")
+        st.subheader("Marco Espinoza")
         st.image(".\imagenes\marco.jpeg")
-        st.write("Graduado de Estadística de la Universidad de Costa Rica. Con dos años de experiencia laboral en la consultoria en datos.")
+        st.write("Estadístico con dos años de experiencia laboral en el mundo de la consultoría en análisis de datos. Apasionado por la investigación")
     with col2:
-        st.subheader("M.Sc Humberto Espinoza")
-        st.write("Con más de 30 años de experiencia en el área médica (...)")
+        st.subheader("Humberto Espinoza")
+        st.write("Médico veterinario y epidemiólogo.")
     # Misión
     st.header('Misión')
     st.write('Nuestro propósito es ser un agente mediador entre la estadística y el o la investigadora para traer una armonía metodológica y que a su vez, sea comunicada de la mejor manera.')
@@ -32,11 +37,11 @@ with tab2:
     st.header('Valores')
     st.write('Como empresa familiar, nuestros valores significan lo que nos ha caracterizado por generaciones. Estos valores no solo definen quiénes somos como empresa, sino que también informan nuestras acciones y decisiones en cada interacción con nuestros clientes, socios y colegas. Con disciplina, organización y excelencia como pilares fundamentales, estamos comprometidos a alcanzar nuevas alturas de éxito y hacer una diferencia significativa en el mundo que nos rodea.')
     st.subheader('Disciplina')
-    st.write('La disciplina es el cimiento sobre el cual construimos nuestro éxito. Nos comprometemos a mantenernos enfocados en nuestras metas y objetivos, incluso cuando los desafíos parezcan insuperables. A través de la disciplina, demostramos nuestra dedicación y determinación para alcanzar la excelencia en todo lo que hacemos.')
+    st.write('La disciplina es el cimiento sobre el cual construimos nuestro éxito. Con la que demostramos nuestra dedicación y determinación para alcanzar la excelencia en todo lo que hacemos.')
     st.subheader('Organización')
-    st.write('La organización es la clave para la eficiencia y la efectividad. Nos esforzamos por mantener un entorno de trabajo ordenado y estructurado, donde cada tarea y proceso esté claramente definido y coordinado. Al priorizar la organización, optimizamos nuestro rendimiento y garantizamos resultados consistentes y confiables.')
+    st.write('Nos esforzamos por mantener un entorno de trabajo ordenado y estructurado, donde cada tarea y proceso esté claramente definido y coordinado. Al priorizar la organización, optimizamos nuestro rendimiento y garantizamos resultados consistentes y confiables.')
     st.subheader('Excelencia')
-    st.write('La excelencia es nuestro estándar. Buscamos constantemente superar las expectativas y alcanzar la más alta calidad en cada proyecto, producto o servicio que entregamos. Nos negamos a conformarnos con lo ordinario, y en su lugar, aspiramos a la grandeza en todo lo que emprendemos.')
+    st.write('Buscamos constantemente superar las expectativas y alcanzar la más alta calidad en cada proyecto que entregamos y siempre aspiramos a la grandeza en todo lo que emprendemos.')
 with tab3:
 
     with st.form('contacto'):
